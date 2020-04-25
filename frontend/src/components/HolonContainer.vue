d48cf301b8c9843b26e952dd1a3139723fb96cbb
Maija <hello@maijagrudule.com>
2020-04-25 23:54:40 +0200

    ADD: grid layout

--- frontend/src/components/HolonContainer.vue
+++ frontend/src/components/HolonContainer.vue
@@ -1,6 +1,46 @@
 <template>
   <div>
     <h1 v-if="teamName" class="text-5xl my-4 text-white">{{ teamName }}</h1>
+    <div class="m-grid-outer">
+      <transition-group
+        class="m-grid-container"
+        :class="{ 'grid-gap': this.layout == 3, 'row-4 c-3': this.hidden }"
+        name="gridmove-move"
+      >
+        <div class="circle row-4 c-3  c-search-outer" key="fixed-possition">
+          <div class="c-inner text-white text-2xl">
+            {{ teamName }}
+          </div>
+        </div>
+        <div
+          v-for="(member, index) in teamMembers"
+          class="circle"
+          :class="getCircleClass(index)"
+          :key="member.name"
+        >
+          <div class=" c-inner profile-card">
+            <div class="px-6 py-4">
+              <h2 class="font-bold text-xl mb-2">{{ member.name }}</h2>
+              <small>{{ member.address }} </small>
+            </div>
+            <div class="">
+              <span
+                class="inline-block bg-gray-200  rounded-full px-3 py-1 text-sm font-semibold text-gray-700 m-2"
+              >
+                ❤️ {{ member.remainingvotes }} remaining
+              </span>
+
+              <button
+                class="bg-blue-500 hover:bg-blue-700 text-white rounded-full px-3 py-1 text-sm font-semibold m-2"
+                @click="openAddLoveModal(index)"
+              >
+                send ❤️
+              </button>
+            </div>
+          </div>
+        </div>
+      </transition-group>
+    </div>
     <div v-if="teamMembers" class="flex mb-4 justify-center">
       <div
         v-for="(member, index) in teamMembers"
@@ -82,6 +122,21 @@ export default {
       user: null,
       TEAMABI: abi,
       address: "0x63aef31d8b104eb9b6c189a513cc2f5efa3dee75",
+      circleClass: [
+        "row-2 c-3",
+        "row-3 c-4",
+        "row-5 c-4",
+        "row-6 c-3",
+        "row-5 c-2",
+        "row-3 c-2",
+
+        "row-1 c-3",
+        "row-2 c-5",
+        "row-6 c-5",
+        "row-7 c-3",
+        "row-6 c-1",
+        "row-2 c-1",
+      ],
     };
   },
   methods: {
@@ -167,6 +222,9 @@ export default {
       this.addLoveModal.amount = 1;
       this.addLoveModal.maxAmount = 100;
       this.showAddLoveModal = false;
+    },
+    getCircleClass(index) {
+      return this.circleClass[index];
     },
   },
   mounted() {
